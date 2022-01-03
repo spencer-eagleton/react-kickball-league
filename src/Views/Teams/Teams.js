4a5adfacdf2d096498e97f67d693d64bfb4533c5
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-03 15:19:30 -0800

    created teams useEffect and logged teams

--- src/Views/Teams/Teams.js
+++ src/Views/Teams/Teams.js
@@ -1,5 +1,16 @@
+import { useState, useEffect } from 'react';
+import { fetchTeams } from '../../services/teams';
+
 export default function Teams() {
   const [teams, setTeams] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchTeams();
 
+      setTeams(data);
+    };
+    fetchData();
+  }, []);
+  console.log(teams);
   return <div>teams</div>;
 }
