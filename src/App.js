3d10ca83bede6959a61498b5eb0f480cd1daeb59
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-02 19:14:59 -0800

    added Home link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
-
+import Teams from './Views/Teams/Teams';
+import Players from './Views/Players/Players';
+import Home from './Views/Home/Home';
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
         <NavLink to="/teams">Teams</NavLink>
         <NavLink to="/players">Players</NavLink>
       </header>
       <Switch>
-        <Route></Route>
-        <Route></Route>
+        <Route component={Home} exact />
+        <Route component={Teams} />
+        <Route component={Players} />
       </Switch>
     </BrowserRouter>
   );
