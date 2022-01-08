0a3b2de55214e0c8b8080d70d150aa9c0ff93e0d
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-07 19:12:13 -0800

    html changes

--- src/Components/TeamDetail/TeamDetail.js
+++ src/Components/TeamDetail/TeamDetail.js
@@ -2,10 +2,10 @@ import PlayerList from '../PlayerList/PlayerList';
 export default function TeamDetail({ team }) {
   return (
     <>
-      <p key={team.id}>
-        <h1>{team.name}</h1>
-        <h3>{team.city}</h3>
-      </p>
+      <p key={team.id}></p>
+      <h1>{team.name}</h1>
+      <h3>{team.city}</h3>
+
       <h4>Roster:</h4>
       {team.players.map((player) => (
         <PlayerList key={player.id} {...player} />
